c8fe3b403431515fe1006f4b863cf65f71b0743f
dragg54 <ajibolasadiq@yahoo.com>
2022-06-24 18:23:06 +0100

    added search container

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../header/Header";
-import { FiArrowRight } from "react-icons/fi"
+import { FiArrowRight } from "react-icons/fi";
 import {
   ButtonContainer,
   HeroCaption,
@@ -14,32 +14,48 @@ import {
   PrimaryButton,
   SecondaryButton,
 } from "./StyledHero";
+import SearchContainer from "../searchContainer/SearchContainer";
 
 const Hero = () => {
   return (
-    <HeroWrapper>
-      <HeroContainer>
-        <Header />
-        <HeroSection>
-          <HeroSubSection>
-            <HeroCaption>
-              We are capable to help you find your  dream home.
-            </HeroCaption>
-            <HeroSubCaption>
-              Making the search for either residential or commercial apartments
-              easier than you can ever imagine, at affordable prices.
-            </HeroSubCaption>
-            <ButtonContainer>
-              <PrimaryButton>Contact Us</PrimaryButton>
-              <SecondaryButton>Learn More <FiArrowRight style={{color: "#E34234", fontSize:"1.2rem", marginLeft:"2px"}}/></SecondaryButton>
-            </ButtonContainer>
-          </HeroSubSection>
-          <HeroImageContainer>
-            <Img src="../../../images/hero-image.png" height={630} alt="" />
-          </HeroImageContainer>
-        </HeroSection>
-      </HeroContainer>
-    </HeroWrapper>
+    <>
+      <HeroWrapper>
+        <div styles={{height:"100%", width:"100%", position:"relative", overflow:"hidden", backgroundColor:"yellow"}}>
+          <HeroContainer>
+            <Header />
+            <HeroSection>
+              <HeroSubSection>
+                <HeroCaption>
+                  We are capable to help you find your dream home.
+                </HeroCaption>
+                <HeroSubCaption>
+                  Making the search for either residential or commercial
+                  apartments easier than you can ever imagine, at affordable
+                  prices.
+                </HeroSubCaption>
+                <ButtonContainer>
+                  <PrimaryButton>Contact Us</PrimaryButton>
+                  <SecondaryButton>
+                    Learn More{" "}
+                    <FiArrowRight
+                      style={{
+                        color: "#E34234",
+                        fontSize: "1.2rem",
+                        marginLeft: "2px",
+                      }}
+                    />
+                  </SecondaryButton>
+                </ButtonContainer>
+              </HeroSubSection>
+              <HeroImageContainer>
+                <Img src="../../../images/hero-image.png" height={630} alt="" />
+              </HeroImageContainer>
+            </HeroSection>
+          </HeroContainer>
+        </div>
+        <SearchContainer />
+      </HeroWrapper>
+    </>
   );
 };
 
