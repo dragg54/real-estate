56b818d3a855ed1114b16b038247d6275ad5273e
dragg54 <ajibolasadiq@yahoo.com>
2022-06-25 21:01:24 +0100

    added properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React, { useState, useEffect } from "react";
 import Hero from "./components/hero/Hero";
+import { fetchApi } from "./API/fetchApi";
+import Properties from "./components/properties/Properties";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    setData(fetchApi());
+  }, []);
+  console.log(data);
   return (
-    <Hero />
+    <>
+      <Hero />
+      <Properties />
+    </>
   );
 }
 
