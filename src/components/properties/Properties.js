c83992a5ec97bbd37233e753cdbb5e770506e051
dragg54 <ajibolasadiq@yahoo.com>
2022-06-26 20:41:41 +0100

    added content

--- src/components/properties/Properties.js
+++ src/components/properties/Properties.js
@@ -1,23 +1,47 @@
 import React from "react";
-import { DescriptionContainer, ImageContainer, PropertyCard, PropertyContainer, PropertyHeader, PropertyWrapper } from "./StyledProperties";
+import {
+  DescriptionContainer,
+  ImageContainer,
+  Img,
+  LocDetailsContainer,
+  PriceHeader,
+  PropertyCard,
+  PropertyContainer,
+  PropertyHeader,
+  PropertyWrapper,
+} from "./StyledProperties";
 
-const Properties = () => {
+const Properties = ({ propertiesData }) => {
   return (
     <PropertyWrapper>
       <PropertyHeader>Our Available Homes</PropertyHeader>
       <PropertyContainer>
-        <PropertyCard>
-          <ImageContainer>rge</ImageContainer>
-          <DescriptionContainer>gge</DescriptionContainer>
-        </PropertyCard>
-        <PropertyCard>
-          <ImageContainer>ege</ImageContainer>
-          <DescriptionContainer>eey</DescriptionContainer>
-        </PropertyCard>
-        <PropertyCard>
-          <ImageContainer>yey</ImageContainer>
-          <DescriptionContainer>he5y</DescriptionContainer>
-        </PropertyCard>
+        {propertiesData &&
+          propertiesData.hits &&
+          propertiesData.hits.splice(0, 3).map((property) => {
+            console.log(property);
+            return (
+              <PropertyCard key={property.id} s>
+                <ImageContainer>
+                  <Img
+                    src={property.coverPhoto.url}
+                    height={500}
+                    width={500}
+                    alt="images"
+                  />
+                </ImageContainer>
+                <DescriptionContainer>
+                  <LocDetailsContainer>
+                   {/*  {property.location.map((locationName) => {
+                      return <p key={locationName.id} style={{fontSize:"1rem"}}>{locationName.name}</p>;
+                    })} */}
+                    <p>{property.title.length>40? property.title.substring(0,35)+ "...": property.title}</p>
+                  </LocDetailsContainer>
+                    <PriceHeader>${property.price / 10}/mo</PriceHeader>
+                </DescriptionContainer>
+              </PropertyCard>
+            );
+          })}
       </PropertyContainer>
     </PropertyWrapper>
   );
--- src/components/properties/StyledProperties.js
+++ src/components/properties/StyledProperties.js
@@ -7,24 +7,49 @@ export const PropertyWrapper = styled.div`
     justify-content: center;
     padding: 28px 140px 0px 140px;
     max-width: 100%;
-    height: 800px;
-    background-color: rgba(0,0,255,0.1);
+    height: 1000px;
     margin-bottom: 100px;
+
+    @media(max-width: 1200px){
+        padding: 22px 90px 0px 90px;
+    }
+    @media(max-width: 900px){
+        padding: 35px 50px 0px 35px
+    }
+    @media(max-width: 768px){
+        padding: 16px 30px 0px 30px;
+        height: 900px;
+    }
+    @media(max-width: 600px){
+        padding: 3px 15px 40px 15px;
+        height: 2200px;
+    }
 `
 export const PropertyHeader = styled.h1`
     font-size: 2.1rem;
     margin-bottom:30px;
+    white-space: nowrap;
+
+    @media(max-width: 600px){
+        font-size: 1.6rem;
+    }
 `
 export const PropertyContainer= styled.div`
     width: 100%;
-    height: 600px;
+    height: 1000px;
     display: flex;
     justify-content: space-between;
-    padding:0
+    padding:0;
+
+    @media(max-width: 600px){
+        height: 100%;
+        flex-direction: column;
+        align-items: center;
+    }
 
 `
 export const PropertyCard = styled.div`
-   width:33%;
+   width:32.5%;
    height: 100%;
    display: flex;
    flex-direction: column;
@@ -33,14 +58,44 @@ export const PropertyCard = styled.div`
    margin-right:5px;
    background-color: brown;
 
+   @media(max-width: 600px){
+        width: 100%;
+        height: 33%;
+    }
+
 `
 export const ImageContainer = styled.div`
     width: 100%;
     height:50%;
-    background-color: purple;
+    object-fit: contain;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    overflow: hidden;
 `
 export const DescriptionContainer=styled.div`
     width:100%;
     height:50%;
-    background-color: brown;
+    background-color: #fff;
+    padding:4px 0px;
+    box-sizing: border-box;
+    padding: 4px 7px;
 ` 
+export const Img = styled.img`
+    height: 100%;
+    width: 100%
+`
+
+export const LocDetailsContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color:rgba(0,0,1,0.5 );
+
+`
+
+export const PriceHeader = styled.h4`
+    font-size: 1.7rem;
+    color:rgba(0,0,1,0.5 );
+`
