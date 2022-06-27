ce441e588b4afb57a04471c736df6a374924715c
dragg54 <ajibolasadiq@yahoo.com>
2022-06-27 21:37:09 +0100

    added icons

--- src/components/properties/Properties.js
+++ src/components/properties/Properties.js
@@ -1,8 +1,12 @@
 import React from "react";
 import {
   DescriptionContainer,
+  GeographyCont,
+  Icons,
   ImageContainer,
   Img,
+  LocationInfo,
+  LocationInfoContainer,
   LocDetailsContainer,
   PriceHeader,
   PropertyCard,
@@ -11,7 +15,12 @@ import {
   PropertyWrapper,
 } from "./StyledProperties";
 
+import { FaBed, FaBath } from "react-icons/fa";
+import { AiOutlineAreaChart } from "react-icons/ai";
+import { MdLocationOn } from "react-icons/md";
+
 const Properties = ({ propertiesData }) => {
+  console.log(propertiesData);
   return (
     <PropertyWrapper>
       <PropertyHeader>Our Available Homes</PropertyHeader>
@@ -31,13 +40,56 @@ const Properties = ({ propertiesData }) => {
                   />
                 </ImageContainer>
                 <DescriptionContainer>
-                  <LocDetailsContainer>
-                   {/*  {property.location.map((locationName) => {
-                      return <p key={locationName.id} style={{fontSize:"1rem"}}>{locationName.name}</p>;
-                    })} */}
-                    <p>{property.title.length>40? property.title.substring(0,35)+ "...": property.title}</p>
-                  </LocDetailsContainer>
+                  <div
+                    style={{
+                      display: "flex",
+                      height: "100%",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <LocDetailsContainer>
+                      <Icons>
+                        <MdLocationOn />
+                      </Icons>
+                      {property.location.map((locationName) => {
+                        return (
+                          <div
+                            key={locationName.id}
+                            style={{ fontSize: "0.8rem", display: "flex" }}
+                          >
+                            <p style={{ whiteSpace: "no-wrap" }}>
+                              {locationName.level == 2 && locationName.name}
+                            </p>
+                          </div>
+                        );
+                      })}
+                    </LocDetailsContainer>
+                    {/*  <p>
+                      {property.title.length > 40
+                        ? property.title.substring(0, 35) + "..."
+                        : property.title}
+                      </p> */}
+                    <LocationInfoContainer>
+                      <LocationInfo>
+                        <Icons>{<FaBed />}</Icons>
+                        {property.rooms} beds
+                      </LocationInfo>
+                      <LocationInfo>
+                        <Icons>{<FaBath />}</Icons>
+                        {property.baths} baths
+                      </LocationInfo>
+                      <LocationInfo>
+                        <Icons>{<AiOutlineAreaChart />}</Icons>
+                        {property.baths * 1000} sqft
+                      </LocationInfo>
+                    </LocationInfoContainer>
                     <PriceHeader>${property.price / 10}/mo</PriceHeader>
+                    <GeographyCont>
+                      lat {+property.geography.lat.toFixed(3)} | lng{" "}
+                      {+property.geography.lng.toFixed(3)}
+                    </GeographyCont>
+                  </div>
                 </DescriptionContainer>
               </PropertyCard>
             );
