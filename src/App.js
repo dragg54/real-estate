49eed2556698b76f541e194009c1f14931b9f2e3
dragg54 <ajibolasadiq@yahoo.com>
2022-07-05 13:13:24 +0100

    loading animation

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ import React, { useState, useEffect } from "react";
 import Hero from "./components/hero/Hero";
 import { baseUrl } from "./URL/baseUrl";
 import Properties from "./components/properties/Properties";
-import axios from "axios"
+import axios from "axios";
 import Services from "./components/services/Services";
 import Testimonials from "./components/testimonials/Testimonials";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
+import Loading from "./components/loading/Loading";
 
 function App() {
   const [data, setData] = useState();
 
   useEffect(() => {
-      axios.request(baseUrl).then(function (response) {
-       setData(response.data)
-      }).catch(function (error) {
+    axios
+      .request(baseUrl)
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
         console.error(error);
       });
   }, []);
-  return (
-    <>
-      <Hero />
-      <Properties propertiesData={data} />
-      <Services />
-      <Testimonials />
-      <Footer />
-    </>
-  );
+  if (data) {
+    return (
+      <>
+        <Hero />
+        <Properties propertiesData={data} />
+        <Services />
+        <Testimonials />
+        <Footer />
+      </>
+    );
+  } else {
+    return <Loading />
+  }
 }
 
 export default App;
