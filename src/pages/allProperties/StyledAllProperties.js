d722e03ffb82df7758e60094184ac7e955780dac
dragg54 <ajibolasadiq@yahoo.com>
2022-07-14 23:43:52 +0100

    increased the height

--- src/pages/allProperties/StyledAllProperties.js
+++ src/pages/allProperties/StyledAllProperties.js
@@ -1,11 +1,9 @@
 import styled from "styled-components";
 import Header from "../../components/header/Header";
-import { NavContainer } from "../../components/header/StyledHeader";
 import { DescriptionContainer, ImageContainer, PropertyCard } from "../../components/properties/StyledProperties";
 
 export const AllPropertiesWrapper = styled.div`
-    background-color: yellow;
-    height:1500px;
+    height:3400px;
     width: 100%;
     padding:50px;
 
@@ -19,7 +17,7 @@ export const AllPropertiesWrapper = styled.div`
     }
 
     @media(max-width: 600px){
-        height: 4200px;
+        height: 6000px;
     }
 `
 
@@ -28,7 +26,7 @@ export const AllPropertiesContainer = styled.div`
     height: 100%;
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
+    grid-gap: 50px;
 
     @media(max-width: 900px){
         grid-template-columns: repeat(2, 1fr);
@@ -42,18 +40,14 @@ export const AllPropertiesContainer = styled.div`
 `
 
 export const AllPropertiesCard = styled(PropertyCard)`
-    background-color: blue;
     width: 100%;
     height: 100%
 `
 
 export const PropertyImageContainer = styled(ImageContainer)`
-    background-color: green;
 `
 export const PropertyDescriptionContainer = styled(DescriptionContainer)`
-    background-color: purple;
 `
 
 export const AllPropertiesHeader = styled(Header)`
-    background-color: blue
 `
