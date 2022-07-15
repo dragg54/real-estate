f49710d6373bfa0bde01a0736e9d9386ac0be39b
dragg54 <ajibolasadiq@yahoo.com>
2022-07-15 11:10:15 +0100

    increased  the mobile height

--- src/pages/allProperties/StyledAllProperties.js
+++ src/pages/allProperties/StyledAllProperties.js
@@ -1,56 +1,71 @@
 import styled from "styled-components";
 import Header from "../../components/header/Header";
-import { DescriptionContainer, ImageContainer, PropertyCard } from "../../components/properties/StyledProperties";
+import {
+  DescriptionContainer,
+  ImageContainer,
+  PropertyCard,
+} from "../../components/properties/StyledProperties";
 
 export const AllPropertiesWrapper = styled.div`
-    height:2000px;
-    width: 100%;
-    padding:50px;
+  height: 2300px;
+  width: 100%;
+  padding: 50px;
 
-    @media(max-width: 1200px){
-        padding: 30px;
-    }
+  @media (max-width: 1200px) {
+    padding: 30px;
+  }
 
-    
-    @media(max-width: 780px){
-        padding: 20px;
-    }
+  @media (max-width: 900px) {
+    height: 3000px;
+    padding: 20px;
+  }
 
-    @media(max-width: 600px){
-        height: 5500px;
-        padding: 10px;
-        margin-bottom: 50px
-    }
-`
+  @media (max-width: 780px) {
+    padding: 20px;
+  }
+
+  @media (max-width: 600px) {
+    height: 5800px;
+    margin-bottom: 50px;
+  }
+`;
 
 export const AllPropertiesContainer = styled.div`
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 50px;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
 
-    @media(max-width: 900px){
-        grid-template-columns: repeat(2, 1fr);
-    }
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-    @media(max-width: 600px){
-       display: flex;
-       flex-direction: column;
-        align-items: center;
-        justify-content: space-between;
-    }
-`
+  @media (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
 
 export const AllPropertiesCard = styled(PropertyCard)`
-    width: 100%;
-    height: 400px
-`
+  width: 100%;
+
+  @media (max-width: 600px) {
+    height: 450px;
+  }
+`;
 
 export const PropertyImageContainer = styled(ImageContainer)`
-`
+  @media (max-width: 600px) {
+    height: 61%;
+  }
+`;
 export const PropertyDescriptionContainer = styled(DescriptionContainer)`
-`
+  @media (max-width: 600px) {
+    height: 39%;
+  }
+`;
 
-export const AllPropertiesHeader = styled(Header)`
-`
+export const AllPropertiesHeader = styled(Header)``;
